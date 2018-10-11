a502386bdbe4b5e37ecd4aa0fedec97b7a20061a
algorythm <anders@wiberg.tech>
2018-10-11 16:51:32 +0200

    Don't ask if we trust the server - we do

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -5,7 +5,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 (
   cd "$DIR/.." # Go to project dir.
 
-  ssh $SSH_USERNAME@$SSH_HOSTNAME -p SSH_PORT <<-EOF
+  ssh $SSH_USERNAME@$SSH_HOSTNAME -o StrictHostKeyChecking=no -p SSH_PORT <<-EOF
     cd $SSH_PROJECT_FOLDER
     git pull
     docker-compose down
